debb6bf85e423e002cfdc85305f3225301b66c26
Dariusramnath <dariusramnath@yahoo.com>
2018-07-17 16:52:17 -0400

    Started firebase integration in App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
+
 class App extends Component {
+componentWillMount() {
+    firebase.initializeApp({
+        apiKey: "AIzaSyB9rNMdgAxKpV_gJpGB457pAJsgNnf_iQU",
+        authDomain: "login-for-crc.firebaseapp.com",
+        databaseURL: "https://login-for-crc.firebaseio.com",
+        projectId: "login-for-crc",
+        storageBucket: "login-for-crc.appspot.com",
+        messagingSenderId: "248893918733"
+      };
+
+    )
+}
+
     render() {
         return (
             <View>

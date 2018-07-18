eecc3e9751c63fd3d0d67c84a864a13b49498620
Dariusramnath <dariusramnath@yahoo.com>
2018-07-18 15:07:27 -0400

    reverted back to before firebase

--- src/App.js
+++ src/App.js
@@ -1,34 +1,17 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
-import firebase from 'firebase';
-import { Header } from './components/common';
+import { View, Text } from 'react-native';
 
 
 class App extends Component {
-componentWillMount() {
-    firebase.initializeApp({
-        apiKey: "AIzaSyB9rNMdgAxKpV_gJpGB457pAJsgNnf_iQU",
-        authDomain: "login-for-crc.firebaseapp.com",
-        databaseURL: "https://login-for-crc.firebaseio.com",
-        projectId: "login-for-crc",
-        storageBucket: "login-for-crc.appspot.com",
-        messagingSenderId: "248893918733"
-      };
-
-    )
-}
-
     render() {
         return (
             <View>
-                <Header  headerText="Welcome to the Cocoa Research Center" />
                 <Text>
-                    Hello World!
+                    Kiki, do you love me?
                 </Text>
             </View>
-        );
+        )
     }
-}
-
+};
 
 export default App;

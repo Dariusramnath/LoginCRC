3d8ddcc1af59dae0bb6c2cf11dd3f0e3a86c63e2
Dariusramnath <dariusramnath@yahoo.com>
2018-07-18 15:18:42 -0400

    App working before firebase

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-
+import {Header} from './components/common/';
 
 class App extends Component {
     render() {
         return (
             <View>
+                <Header headerText="Welcome to the Cocoa Research Center" />
                 <Text>
                     Kiki, do you love me?
                 </Text>
             </View>
-        )
+        );
     }
 };
 

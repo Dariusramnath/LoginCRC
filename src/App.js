85266b84a17d6b7f2e2bc98bde33ad685c130838
Dariusramnath <dariusramnath@yahoo.com>
2018-07-18 15:19:19 -0400

    App working before firebase

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import {Header} from './components/common/';
+import { Header } from './components/common/';
 
 class App extends Component {
     render() {

3d8fb6ff828dd0e54ed05aab185607196760bc43
Dariusramnath <dariusramnath@yahoo.com>
2018-07-18 15:27:28 -0400

    Firebase up with no problems

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Header } from './components/common/';
+import firebase from 'firebase';
 
 class App extends Component {
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: "AIzaSyB9rNMdgAxKpV_gJpGB457pAJsgNnf_iQU",
+            authDomain: "login-for-crc.firebaseapp.com",
+            databaseURL: "https://login-for-crc.firebaseio.com",
+            projectId: "login-for-crc",
+            storageBucket: "login-for-crc.appspot.com",
+            messagingSenderId: "248893918733"
+          }
+        );
+    }
+
+
+
     render() {
         return (
             <View>

5aaf8c9cd58223017006a5b58948c01e172e059f
Dariusramnath <dariusramnath@yahoo.com>
2018-07-23 16:39:20 -0400

    startup of firebase authentication with error handling

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,26 +1,54 @@
 import React, { Component } from 'react';
+import firebase from 'firebase';
+import { Text } from 'react-native';
 import { Button, Card, CardSection, Input } from './common'
 
 
 class LoginForm extends Component {
-    state = { text: '' };
+    state = { email: '', password: '', error: '' };
+
+    onButtonPress() {
+        const { email, password } = this.state;
+
+        this.setState({ error: '' })
+
+        firebase.auth().signInWithEmailAndPassword( email, password )
+        .catch(() => {
+            firebase.auth(). createUserWithEmailAndPassword( email, password )
+            .catch(() => {
+                this.setState({ error: "Authentication Failed. This email is already in use"})
+            }); 
+        });
+    }
 
     render() {
         return (
                 <Card>
                     <CardSection>
                         <Input 
-                        value={ this.state.text }
-                        onChangeText={ text => this.setState({ text })}
+                        placeholder="user@gmail.com"
+                        label='Email'
+                        value={ this.state.email }
+                        onChangeText={ email => this.setState({ email})}
                         />
                     </CardSection>
 
                     <CardSection>
-                        <Input />
+                        <Input 
+                        secureTextEntry={true}
+                         placeholder="password"
+                         label="Password"
+                         value={ this.state.password }
+                         onChangeText={ password => this.setState({ password })}
+                        />
                     </CardSection>
 
+                    <Text style={styles.errorTextStyle} >
+                        {this.state.error}
+                    </Text>
+
                     <CardSection>
-                        <Button>
+                        <Button onPress={this.onButtonPress.bind(this)} >
                             Log In
                         </Button>
                     </CardSection>
@@ -29,6 +57,14 @@ class LoginForm extends Component {
         )
     }
 };
+
+const styles = {
+    errorTextStyle: {
+        fontSize: 20,
+        alignItems: 'center',
+        color: 'red'
+    }
+}
    
 
 export default LoginForm;
--- src/components/common/Input.js
+++ src/components/common/Input.js
@@ -2,11 +2,18 @@ import React from 'react';
 import { TextInput, View, Text } from 'react-native';
 
 
-const Input = ({ label, value, onChangeText }) => {
+const Input = ({ label, value, onChangeText, placeholder, secureTextEntry }) => {
+    const { input1, labelStyle, containerStyle } = styles;
+
     return (
-        <View>
-            <Text>{label}</Text>
+        <View style={containerStyle}>
+            <Text style={labelStyle}>{label}</Text>
             <TextInput 
+                secureTextEntry={secureTextEntry}
+                placeholder={placeholder}
+                autoCorrect={false}
+                underlineColorAndroid= 'transparent'
+                style={input1}
                 value={value}
                 onChangeText={onChangeText}
                 style={styles.input1}
@@ -17,9 +24,24 @@ const Input = ({ label, value, onChangeText }) => {
 
 const styles = {
     input1: {
-        width: 400,
+        color: '#000',
+        paddingRight: 5,
+        paddingLeft: 5,
+        fontSize: 18,
+        lineHeight: 23,
+        flex: 2, 
+    },
+    labelStyle: {
+        fontSize: 18,
+        paddingLeft: 5,
+        flex: 1
+    },
+    containerStyle: {
+        flex: 1,
         height: 40,
-    }
-}
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+};
 
 export { Input }

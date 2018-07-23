5c0d805b45dee2a9943d3bffad5d570b6c4e7b74
Dariusramnath <dariusramnath@yahoo.com>
2018-07-23 18:13:50 -0400

    Added Log Out button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import { Header } from './components/common/';
+import { Header, Button, Spinner } from './components/common/';
 import firebase from 'firebase';
 import LoginForm from './components/LoginForm'
 
 class App extends Component {
+    state = { loggedIn: null };
+
     componentWillMount() {
         firebase.initializeApp({
             apiKey: "AIzaSyB9rNMdgAxKpV_gJpGB457pAJsgNnf_iQU",
@@ -13,17 +15,42 @@ class App extends Component {
             projectId: "login-for-crc",
             storageBucket: "login-for-crc.appspot.com",
             messagingSenderId: "248893918733"
-          }
-        );
+          });
+
+          firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                this.setState({ loggedIn: true });
+            } else {
+                this.setState({ loggedIn: false });
+            }
+          });
+
     }
 
+    renderContent() {
+        switch (this.state.loggedIn) {
+            case true:
+                return (
+                <View style={{ flexDirection: 'row', paddingTop: 10}} >
+                    <Button onPress={() => firebase.auth().signOut()}>
+                    Log Out
+                    </Button>
+                </View>
+                );
+            case false:
+             return <LoginForm />;
+            default:
+                return <Spinner size='large' />;
+        }
+    }
 
+    
 
     render() {
         return (
             <View>
                 <Header headerText="Welcome to the Cocoa Research Center" />
-                <LoginForm />
+                {this.renderContent()}
             </View>
         );
     }

c41b8960f5f9e85fd50b2a9886738ba25b800dd1
Dariusramnath <dariusramnath@yahoo.com>
2018-08-07 12:21:15 -0400

    i dont know

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Header, Button, Spinner } from './components/common/';
 import firebase from 'firebase';
-import LoginForm from './components/LoginForm'
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
     state = { loggedIn: null };
